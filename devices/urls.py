c682336830db9ae4f8d2d1455e400427092c2669
Shrey <shreyshukla1010@gmail.com>
2024-08-20 18:56:56 +0530

    add: postman collection

--- devices/urls.py
+++ devices/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('api/devices/', views.list_devices, name='list_devices'),
-    path('api/devices/', views.create_device, name='create_device'),
+    path('api/add/devices/', views.create_device, name='create_device'),
     path('api/devices/<str:device_uid>/', views.retrieve_device, name='retrieve_device'),
-    path('api/devices/<str:device_uid>/', views.delete_device, name='delete_device'),
+    path('api/devices/delete/<str:device_uid>/', views.delete_device, name='delete_device'),
     path('api/devices/<str:device_uid>/readings/<str:parameter>/', views.list_readings, name='list_readings'),
     path('devices-graph/', views.device_graph, name='device_graph'),
 ]
